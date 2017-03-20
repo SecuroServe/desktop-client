c06cebff058a7a000b4920333b1a315f4acc7b28
Marc <mjgroos@live.nl>
2017-03-20 14:44:23 +0100

    Login form created

--- src/main/java/securoserve/Main.java
+++ src/main/java/securoserve/Main.java
@@ -2,18 +2,29 @@ package securoserve;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+        primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight()) / 2);
     }
 
 
--- src/main/java/securoserve/ui/controller/LoginController.java
+++ src/main/java/securoserve/ui/controller/LoginController.java
@@ -1,4 +1,30 @@
 package securoserve.ui.controller;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    public Button loginButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loginButton.setOnAction(this::handleLoginAction);
+    }
+
+    @FXML
+    public void handleLoginAction(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("test");
+        alert.setContentText("De knop is geklikt.");
+
+        alert.showAndWait();
+    }
 }
