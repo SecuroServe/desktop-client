fdc48fa969727cf3455d59485e3ad0c714832aa0
Marc <mjgroos@live.nl>
2017-04-10 23:18:04 +0200

    Updated Login Form

--- src/main/java/securoserve/ui/controller/LoginController.java
+++ src/main/java/securoserve/ui/controller/LoginController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,18 +14,31 @@ public class LoginController implements Initializable {
 
     @FXML
     public Button loginButton;
+    public Button closeButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         loginButton.setOnAction(this::handleLoginAction);
+        closeButton.setOnAction(this::handleCloseAction);
     }
 
     @FXML
     public void handleLoginAction(ActionEvent event) {
+
+
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("test");
         alert.setContentText("De knop is geklikt.");
 
         alert.showAndWait();
+    }
+
+    @FXML
+    public void handleCloseAction(ActionEvent event) {
+
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+
+        System.exit(0);
     }
 }
