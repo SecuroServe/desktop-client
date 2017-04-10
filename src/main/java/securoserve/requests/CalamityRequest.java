95bde06a3cc8c297d5248e2e5b02d1bcf89f81fb
guillaimejanssen <guillaimejanssen@msn.com>
2017-04-10 20:11:06 +0200

    Added al different url's for calamity Implemented getCalamityById Removed the static columns in the tableview

--- src/main/java/securoserve/requests/CalamityRequest.java
+++ src/main/java/securoserve/requests/CalamityRequest.java
@@ -1,12 +1,11 @@
 package securoserve.requests;
 
-import api.ConfirmationMessage;
-import api.ICalamity;
-import library.Calamity;
-import library.Location;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.methods.HttpPost;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import securoserve.api.interfaces.ConfirmationMessage;
+import securoserve.api.interfaces.ICalamity;
+import securoserve.library.Calamity;
+import securoserve.library.Location;
 
 import java.io.InputStream;
 import java.util.List;
@@ -16,10 +15,36 @@ import java.util.List;
  */
 public class CalamityRequest implements ICalamity {
 
-    HttpPost post;
+    private static final String REQUEST_PREFIX = "http://localhost:8080";
+
+    private static final String GET_ALL = "/allcalamities";
+    private static final String GET_CALAMITY_BY_ID = "/calamitybyid?token={token}&id={id}";
+
+    private static final String POST_ADD_CALAMITY_ASSIGNEE = "/addcalamityassignee?token={token}&" +
+            "calamityid={calamityid}&" +
+            "userid={userid}";
+    private static final String POST_ADD_CALAMITY = "/addcalamity?token={token}&" +
+            "title={title}&" +
+            "message={message}&" +
+            "location={location}&" +
+            "confirmed={confirmed}&" +
+            "closed={closed}";
+
+    private static final String UPDATE_CALAMITY = "/updatecalamity?token={token}&" +
+            "id={id}&" +
+            "name={name}&" +
+            "description={description}&" +
+            "location={location}";
+
+    private static final String DELETE_CALAMITY = "/deletecalamity?token={token}&id={id}";
+    private static final String DELETE_CALAMITY_ASSIGNEE = "/deletecalamityassignee?token={token}&" +
+            "calamityid={calamityid}&" +
+            "userid={userid}";
+
+    RestTemplate restTemplate;
 
     public CalamityRequest(){
-        post = new HttpPost("http://localhost:8080");
+        restTemplate = new RestTemplate();
     }
 
     @Override
@@ -28,17 +53,32 @@ public class CalamityRequest implements ICalamity {
     }
 
     @Override
-    public Calamity calamityById(@RequestParam("token") String s, @RequestParam("id") int i) {
-        return null;
+    public Calamity calamityById(@RequestParam("token") String token, @RequestParam("id") int id) {
+        return restTemplate.getForObject(REQUEST_PREFIX + GET_CALAMITY_BY_ID, Calamity.class, token, id);
     }
 
     @Override
-    public ConfirmationMessage addCalamity(@RequestParam("token") String s, @RequestParam("name") String s1, @RequestParam("description") String s2, @RequestParam("location") Location location) {
+    public ConfirmationMessage addCalamity(String s, String s1, String s2, Location location, boolean b, boolean b1) {
         return null;
     }
 
     @Override
     public ConfirmationMessage updateCalamity(@RequestParam("token") String s, @RequestParam("id") int i, @RequestParam("name") String s1, @RequestParam("description") String s2, @RequestParam("location") Location location) {
+        return null;
+    }
+
+    @Override
+    public ConfirmationMessage deleteCalamity(String s, int i) {
+        return null;
+    }
+
+    @Override
+    public ConfirmationMessage addCalamityAssignee(String s, int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public ConfirmationMessage deleteCalamityAssignee(String s, int i, int i1) {
         return null;
     }
 }
--- src/main/java/securoserve/ui/controller/CalamityListController.java
+++ src/main/java/securoserve/ui/controller/CalamityListController.java
@@ -1,17 +1,21 @@
 package securoserve.ui.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import library.Calamity;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import securoserve.library.Calamity;
+import org.mockito.invocation.Location;
+import securoserve.library.User;
 import securoserve.requests.CalamityRequest;
 
 import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 /**
  * Created by guillaimejanssen on 20/03/2017.
@@ -23,13 +27,18 @@ public class CalamityListController implements Initializable {
     public Button closeBtn;
     @FXML
     public Button refreshButton;
+    @FXML
+    public TableView<Calamity> calamityTable;
 
     private List<Calamity> calamities;
+    private ObservableList<Calamity> obsList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         closeBtn.setOnAction(this::handleCloseAction);
         refreshButton.setOnAction(this::handleRefreshAction);
+
+        initiateTableColumns();
 
         // Refreshing the table every 10 seconds
         Timer timerToRefresh = new Timer();
@@ -44,9 +53,38 @@ public class CalamityListController implements Initializable {
         System.exit(0);
     }
 
+    private void initiateTableColumns() {
+
+        TableColumn calamityName = new TableColumn("Calamity");
+        calamityName.setMinWidth(133);
+        calamityName.setCellValueFactory(new PropertyValueFactory<Calamity, String>("title"));
+
+        TableColumn calamityDate = new TableColumn("Date");
+        calamityDate.setMinWidth(116);
+        calamityDate.setCellValueFactory(new PropertyValueFactory<Calamity, Date>("date"));
+
+        TableColumn calamityLocation = new TableColumn("Location");
+        calamityLocation.setMinWidth(131);
+        calamityLocation.setCellValueFactory(new PropertyValueFactory<Calamity, Location>("location"));
+
+        TableColumn calamityAlertedBy = new TableColumn("Alerted By");
+        calamityAlertedBy.setMinWidth(111);
+        calamityAlertedBy.setCellValueFactory(new PropertyValueFactory<Calamity, User>("user"));
+
+        TableColumn calamityStatus = new TableColumn("Status");
+        calamityStatus.setMinWidth(79);
+        calamityStatus.setCellValueFactory(new PropertyValueFactory<Calamity, Calamity.CalamityState>("state"));
+
+    }
+
     private void refreshCalamityTable() {
         CalamityRequest calamityRequest = new CalamityRequest();
         calamities = calamityRequest.allCalamity();
+
+        obsList.removeAll(obsList);
+
+        obsList = FXCollections.observableArrayList(calamities);
+        calamityTable.setItems(obsList);
     }
 
     class PostRequestTask extends TimerTask {
