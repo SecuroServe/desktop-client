e23c70962afafcd23f835485e17abfa99f9b4d51
Yannic Riesebos <zebybez@surfspot.nu>
2017-04-12 15:53:25 +0200

    made SendRescuer controller for fxml

--- src/main/java/securoserve/ui/controller/SendRescuerController.java
+++ src/main/java/securoserve/ui/controller/SendRescuerController.java
@@ -1,8 +1,88 @@
 package securoserve.ui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by yannic on 12/04/2017.
  */
-public class SendRescuerController {
+public class SendRescuerController implements Initializable {
+    @FXML
+    private Button doneButton;
+    @FXML
+    private Button selectCalamityButton;
+    @FXML
+    private Button addRescuerButton;
+    @FXML
+    private Button removeRescuerButton;
+    @FXML
+    private TableView calamityTableView;
+    @FXML
+    private TableView rescuerTableView;
+    @FXML
+    private ListView selectedRescuerListView;
+    @FXML
+    private Label selectedCalamityLabel;
 
+    public SendRescuerController() {
+        selectedCalamityLabel.setText("test");
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        doneButton.setOnAction(this::finishAction);
+        selectCalamityButton.setOnAction(this::selectCalamity);
+        addRescuerButton.setOnAction(this::addRescuer);
+        removeRescuerButton.setOnAction(this::removeRescuer);
+    }
+
+    /***
+     * adds a rescuer to respond to the selected calamity
+     * updates the listView
+     * @param actionEvent
+     */
+    private void addRescuer(ActionEvent actionEvent) {
+    }
+
+    /***
+     * removes a rescuer from the list of selected rescuers.
+     * updates the listview.
+     * @param actionEvent
+     */
+    private void removeRescuer(ActionEvent actionEvent) {
+
+    }
+
+    /***
+     * selects a calamity to assign the rescuers to.
+     * sets the calamity, and changes the label.
+     * @param actionEvent
+     */
+    private void selectCalamity(ActionEvent actionEvent) {
+    }
+
+    /***
+     * this is a method handling the end of the action
+     * informs the rescuers and sets the database
+     * then closes the screen.
+     * @param actionEvent
+     */
+    private void finishAction(ActionEvent actionEvent) {
+    }
 }
